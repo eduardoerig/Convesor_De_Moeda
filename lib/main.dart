8b553f43b257328b92600ad03469cfadfd7aef11
Eduardo Felipe Storck Erig <137730731+eduardoerig@users.noreply.github.com>
2025-08-26 11:23:02 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,89 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
 import 'conversor.dart';
 import 'taxas.dart';
 
-void main() {
-  print('Conversor de moedas:');
-  print('''
-Escolha a moeda de destino: 
-1 - USD
-2 - EUR
-3 - ARS
-''');
+void main() => runApp(ConversorApp());
 
-  print('Sua escolha: ');
-  var escolha = int.parse(stdin.readLineSync()!);
+class ConversorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Conversor de Moedas',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ConversorHome(),
+    );
+  }
+}
 
-  print('Digite a quantia em R\$ que deseja converter: ');
-  var valor = double.parse(stdin.readLineSync()!);
+class ConversorHome extends StatefulWidget {
+  @override
+  _ConversorHomeState createState() => _ConversorHomeState();
+}
 
-  switch (escolha) {
-    case 1:
-      print('USD');
-      print('O valor em USD é de: ${calcularConversao(valor, taxas['EUA']!).toStringAsFixed(2)}');
-      break;
-    case 2:
-      print('EUR');
-      print('O valor em EUR é de: ${calcularConversao(valor, taxas['EURO']!).toStringAsFixed(2)}');
-      break;
-    case 3:
-      print('ARS');
-      print('O valor em ARS é de: ${calcularConversao(valor, taxas['PESO']!).toStringAsFixed(2)}');
-      break;
-    default:
-      print('Opção inválida.');
+class _ConversorHomeState extends State<ConversorHome> {
+  final TextEditingController _controller = TextEditingController();
+  String _resultado = '';
+  String _moedaSelecionada = 'USD';
+
+  void _converter() {
+    double valor = double.tryParse(_controller.text) ?? 0.0;
+    double? taxa;
+
+    switch (_moedaSelecionada) {
+      case 'USD':
+        taxa = taxas['EUA'];
+        break;
+      case 'EUR':
+        taxa = taxas['EURO'];
+        break;
+      case 'ARS':
+        taxa = taxas['PESO'];
+        break;
+    }
+
+    if (taxa != null) {
+      double convertido = calcularConversao(valor, taxa);
+      setState(() {
+        _resultado = 'Valor em $_moedaSelecionada: ${convertido.toStringAsFixed(2)}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Conversor de Moedas')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Digite o valor em R\$'),
+            ),
+            SizedBox(height: 20),
+            DropdownButton<String>(
+              value: _moedaSelecionada,
+              items: ['USD', 'EUR', 'ARS'].map((moeda) {
+                return DropdownMenuItem(value: moeda, child: Text(moeda));
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _moedaSelecionada = value!;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _converter,
+              child: Text('Converter'),
+            ),
+            SizedBox(height: 20),
+            Text(_resultado, style: TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
+    );
   }
 }
